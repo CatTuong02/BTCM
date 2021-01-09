34c1ed5ebb78957c9e593921769e30523bf461df
CatTuong02 <77189568+CatTuong02@users.noreply.github.com>
2021-01-09 16:12:06 +0700

    Create database script

---
+++ DatabaseScript.sql
@@ -0,0 +1,42 @@
+﻿--SQL Structured Query Language
+
+--DBMS
+
+--Tạo DB
+--Tạo bảng - tạo mối quan hệ giữa các bảng
+--Insert, update, delete, select 
+--=> 
+--C = Create = Insert
+--R = Read = Select
+--U = update = update
+--D = delete = delete
+
+--viết câu truy vấn
+--trigger
+--store procedure
+
+--drop database, drop table, drop khoá ngoại, drop trigger, drop store procedure
+
+
+------ Create database ------
+CREATE DATABASE movie
+GO
+
+USE movie
+GO
+
+------ Drop database ------
+--USE master;
+--ALTER DATABASE movie SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+--DROP DATABASE movie;
+
+------ Drop tables ------
+
+
+------ Create tables ------
+
+
+------ Insert data ------
+
+
+------ Create store procedure ------

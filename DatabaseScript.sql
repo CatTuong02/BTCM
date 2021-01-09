bf263248aafcaa1af4bca731d3c82224a99a539d
CatTuong02 <77189568+CatTuong02@users.noreply.github.com>
2021-01-09 17:15:31 +0700

    Seek data

--- DatabaseScript.sql
+++ DatabaseScript.sql
@@ -138,17 +138,57 @@ ALTER TABLE rating ADD FOREIGN KEY (rev_id) REFERENCES reviewer(rev_id)
 
 ------ Insert data ------
 INSERT INTO actor (act_fname, act_lname, act_gender) VALUES ('A', 'B', 'M')
+INSERT INTO actor (act_fname, act_lname, act_gender) VALUES ('Robert', 'Downey Jr.', 'M')
+INSERT INTO actor (act_fname, act_lname, act_gender) VALUES ('Josh', 'Brolin', 'F')
+INSERT INTO actor (act_fname, act_lname, act_gender) VALUES ('Ngo', 'Thanh Van', 'F')
+INSERT INTO actor (act_fname, act_lname, act_gender) VALUES ('Ly', 'Hai', 'M')
+INSERT INTO actor (act_fname, act_lname, act_gender) VALUES ('Micle', 'Bay', 'M')
+
 
 INSERT INTO movie (mov_title, mov_year, mov_time, mov_lang, mov_dt_rel, mov_rel_country) VALUES ('PHIM A', 2020, 100, 'VN', '2020/1/1', 'VN')
+INSERT INTO movie (mov_title, mov_year, mov_time, mov_lang, mov_dt_rel, mov_rel_country) VALUES ('PHIM HAI PHUONG', 2019, 120, 'VN', '2019/9/10', 'VN')
+INSERT INTO movie (mov_title, mov_year, mov_time, mov_lang, mov_dt_rel, mov_rel_country) VALUES ('PHIM ROM', 2020, 115, 'VN', '2020/7/28', 'VN')
+INSERT INTO movie (mov_title, mov_year, mov_time, mov_lang, mov_dt_rel, mov_rel_country) VALUES ('PHIM BATMAN', 2015, 300, 'USA', '2015/3/14', 'USA')
 
-INSERT INTO director
-INSERT INTO genres
-INSERT INTO reviewer
+
+INSERT INTO director (dir_fname, dir_lname) VALUES ('Nguyen', 'Van Ti')
+INSERT INTO director (dir_fname, dir_lname) VALUES ('Tran', 'Van Teo')
+INSERT INTO director (dir_fname, dir_lname) VALUES ('Anthony', 'Russo')
+
+
+INSERT INTO genres (gen_title) VALUES ('Ma')
+INSERT INTO genres (gen_title) VALUES ('Hai')
+INSERT INTO genres (gen_title) VALUES ('Hanh Dong')
+INSERT INTO genres (gen_title) VALUES ('Tinh Cam')
+
+
+INSERT INTO reviewer (rev_name) VALUES ('Vat vo')
+INSERT INTO reviewer (rev_name) VALUES ('Think view')
+INSERT INTO reviewer (rev_name) VALUES ('TNC Store')
+
 
 INSERT INTO movie_cast (act_id, mov_id, role) VALUES (1, 1, 'Dien vien chinh')
-INSERT INTO movie_direction
-INSERT INTO movie_genres
-INSERT INTO rating
+INSERT INTO movie_cast (act_id, mov_id, role) VALUES (2, 2, 'Dien vien phu')
+INSERT INTO movie_cast (act_id, mov_id, role) VALUES (3, 3, 'Dien vien phu')
+INSERT INTO movie_cast (act_id, mov_id, role) VALUES (4, 4, 'Dien vien chinh')
+INSERT INTO movie_cast (act_id, mov_id, role) VALUES (5, 2, 'Dien vien phu')
+INSERT INTO movie_cast (act_id, mov_id, role) VALUES (6, 4, 'Dien vien chinh')
 
+
+INSERT INTO movie_direction (dir_id, mov_id) VALUES (1, 2)
+INSERT INTO movie_direction (dir_id, mov_id) VALUES (2, 3)
+INSERT INTO movie_direction (dir_id, mov_id) VALUES (3, 4)
+
+
+INSERT INTO movie_genres (mov_id, gen_id) VALUES (1, 1)
+INSERT INTO movie_genres (mov_id, gen_id) VALUES (2, 2)
+INSERT INTO movie_genres (mov_id, gen_id) VALUES (3, 3)
+INSERT INTO movie_genres (mov_id, gen_id) VALUES (4, 4)
+
+
+INSERT INTO rating (mov_id, rev_id, rev_stars, num_o_ratings) VALUES (1, 1, 6, 10)
+INSERT INTO rating (mov_id, rev_id, rev_stars, num_o_ratings) VALUES (2, 1, 6, 15)
+INSERT INTO rating (mov_id, rev_id, rev_stars, num_o_ratings) VALUES (3, 2, 8, 4)
+INSERT INTO rating (mov_id, rev_id, rev_stars, num_o_ratings) VALUES (4, 3, 3, 1)
 
 ------ Create store procedure ------
